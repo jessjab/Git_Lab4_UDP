51c741749372f63372155bd00bfa3574ded38985
jessjab <37161635+jessjab@users.noreply.github.com>
2018-03-07 21:56:02 -0700

    Update UDP_server.py

--- UDP_server.py
+++ UDP_server.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
         CLIENT_NUMBER = CLIENT_DATA_split[1]
         print "Client Number: ", CLIENT_NUMBER
         print "Length of encrypted keys", len(ENCRYPT_KEYS)
-        if CLIENT_NUMBER >= len(ENCRYPT_KEYS):
+        if int(CLIENT_NUMBER) >= len(ENCRYPT_KEYS):
             print "The Key for this client is not available."
             sent = serverSock.sendto(str("The Key for this client is not available."),ADDR)
             print >>sys.stderr, 'sent %s bytes back to %s' % (sent, ADDR)
